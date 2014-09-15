cfe84f5642ea9936d28fbc67f3f513707c50a9b7
Sverre Iversen <sverre.iversen@ngu.no>
2014-09-15 16:32:25 +0200

    Bug fix & added robust handling of images with "media".
    <img/>: class "hidden" added
    mediaqueryload.js: total rewrite of img & link handeling

--- mediaqueryload.js
+++ mediaqueryload.js
@@ -1,29 +1,27 @@
+//function for check of mediaquerydependent styling
 function mediaqueryload(ev) {
-  var queriedresource = document.querySelectorAll('.mediaquerydependent'),
-      all = queriedresource.length,
-      i = all,
-      current = null,
-      attr = null;
-  if (ev && ev.type === 'resize') {
-    while (i--) {
-      current = queriedresource[i];
-      for (attr in current.dataset) {
-        current.removeAttribute(attr);
-      }
-    }
-    i = all;
-  }
-  while (i--) {
-    current = queriedresource[i];
-    if (  current.dataset.media &&
-          window.matchMedia(current.dataset.media).matches) {
-      for (attr in current.dataset) {
-        if (attr !== 'media') {
-          current.setAttribute(attr, current.dataset[attr]);
-        }
-      }
-    }
-  }
+	var mediaqueryobjects = document.querySelectorAll('.mediaquerydependent'),
+	page = document.querySelector(".page"),
+	all = mediaqueryobjects.length, i = all, current = null, attr = null,
+	sDelete = "link" /* "link bla1 bla2" */ , bDelete=true;
+	if (page) page.classList.add('hidden');
+	while (i--) {
+		current = mediaqueryobjects[i];
+		if (current.dataset.media) {
+			bDelete = (sDelete.indexOf((current.tagName).toLowerCase())>-1);
+			if (window.matchMedia(current.dataset.media).matches) {
+				if (bDelete) current.classList.remove("mediaquerydependent");
+				for (attr in current.dataset) {
+					if (attr !== 'media' || bDelete) {
+						current.setAttribute(attr, current.dataset[attr]);
+						current.removeAttribute("data-"+attr);
+					}
+					if (!bDelete) current.classList.remove('hidden');
+				}
+			} else if (!bDelete) current.classList.add('hidden');
+		}
+	}
+	if (page) page.classList.remove('hidden');
 }
 mediaqueryload();
 window.addEventListener('resize', mediaqueryload, false);
