1ebbe9beb6c0ea5b365b58c4bbd3fe2b803571e6
Sverre Iversen <sverre.iversen@ngu.no>
2014-09-18 08:20:01 +0200

    Dynamic mediaquery load
    Added comments

--- mediaqueryload.js
+++ mediaqueryload.js
@@ -1,16 +1,30 @@
 //function for check of mediaquerydependent styling
 function mediaqueryload(ev) {
-	var mediaqueryobjects = document.querySelectorAll('.mediaquerydependent'),
+	
+	//get all tags with style "mediaquery"
+	var mediaqueryobjects = document.querySelectorAll('.mediaquery'),
+	
+	//check to se if a encapsulating div "page" exists
+	//useful when you need to hide old style on slow devices
 	page = document.querySelector(".page"),
+	
 	all = mediaqueryobjects.length, i = all, current = null, attr = null,
+	
+	//specify which tags where deletion of dataset attributes is done when source is loaded first time
 	sDelete = "link" /* "link bla1 bla2" */ , bDelete=true;
+	
+	//turn div "page" off if exists
 	if (page) page.classList.add('hidden');
+	
 	while (i--) {
 		current = mediaqueryobjects[i];
+		
 		if (current.dataset.media) {
 			bDelete = (sDelete.indexOf((current.tagName).toLowerCase())>-1);
+
+			//transform matched tags to normal tags (link,...) and turn on/off other tags according to "media"
 			if (window.matchMedia(current.dataset.media).matches) {
-				if (bDelete) current.classList.remove("mediaquerydependent");
+				if (bDelete) current.classList.remove("mediaquery");
 				for (attr in current.dataset) {
 					if (attr !== 'media' || bDelete) {
 						current.setAttribute(attr, current.dataset[attr]);
@@ -21,6 +35,8 @@ function mediaqueryload(ev) {
 			} else if (!bDelete) current.classList.add('hidden');
 		}
 	}
+	
+	//turn div "page" on if exists to show new style on slow devices
 	if (page) page.classList.remove('hidden');
 }
 mediaqueryload();
